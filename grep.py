d16f5438f67c27cadeea561f56a2d6ef2c6a1a06
Jakub Sláma <slamajak@jupyter.korpus.cz>
2018-03-15 16:55:46 +0100

    Error handling

--- grep.py
+++ grep.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
+"""Usage: grep.py PATTERN FILE
+
+Search for lines in FILE containing PATTERN.
+PATTERN can be regex.
+"""
+# = __doc__
 
 import sys
-import regex as re
+try:
+    import regex as re
+except ModuleNotFoundError:
+    print("regex module not found, using re", file=sys.stderr)
+    import re
 
-pattern, path = sys.argv[1:]
-with open(path) as file:
-    for line in file:
-        if re.search(pattern, line):
-            print(line, end="")
+try:
+    pattern, path = sys.argv[1:]
+except ValueError:
+    print(__doc__.strip(), file=sys.stderr)
+    sys.exit(1)
+
+try:
+    with open(path) as file:
+        for line in file:
+            if re.search(pattern, line):
+                print(line, end="")
+except FileNotFoundError:
+    print("file not found:", path, file=sys.stderr)
+    sys.exit(1)
